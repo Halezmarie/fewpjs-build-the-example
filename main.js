6b634959db9b5171f034a9ab7055d2874f9c4673
halezmarie <teacherhaleymarie@gmail.com>
2021-08-19 10:59:13 -0500

    commit tree a9bae56afd79923a91c282d871af6c358c5847be

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// const body = document.querySelector("body");
+const mediaPosts = document.getElementsByClassName("media-post");
+Array.from(mediaPosts).forEach(function(post) {
+  post.addEventListener("click", handleClick);
+})
 
-
-
+function handleClick(event) {
+  if (event.target.nodeName === "SPAN") {
+    targetId = event.path[4].id; 
+    mimicServerCall()
+    .then(() => {
+      const heart = document.getElementById(targetId).children[2].children[0].children[0].children[0]
+      if (heart.innerHTML === EMPTY_HEART) {
+        heart.innerHTML = FULL_HEART;
+        heart.classList.add("activated-heart");
+      } else {
+        heart.innerHTML = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      };
+    })
+    .catch((error) => {
+      const modal = document.getElementById("modal");
+      modal.classList.remove("hidden");
+      modal.innerHTML += error;
+      setTimeout(removeHidden, 5000);
+      
+      function removeHidden() {
+        modal.classList.add("hidden");
+      }
+    })
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
